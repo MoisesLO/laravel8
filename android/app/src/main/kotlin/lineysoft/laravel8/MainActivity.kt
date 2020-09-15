d81bc43c83935349e1d0a37f55764a5e5ef22d07
moises <elnaufrago2009@gmail.com>
2020-09-15 16:06:18 -0500

    initial commit

---
+++ android/app/src/main/kotlin/lineysoft/laravel8/MainActivity.kt
@@ -0,0 +1,6 @@
+package lineysoft.laravel8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Laravel 8",
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var data;
+
+  Future loadJson() async {
+    String data = await rootBundle.loadString('assets/json/data.json');
+    return json.decode(data);
+  }
+
+  @override
+  void initState() {
+    loadJson().then((value) {
+      setState(() {
+        data = value;
+      });
+    });
+    super.initState();
+    // print(data[1]['titulo']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Laravel 8'),
+        ),
+        body: ListView.builder(
+          shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ExpansionTile(
+                title: Text(data[index]['titulo']),
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                      itemBuilder: (context, i) {
+                        return ListTile(title: Text('hols'));
+                      },
+                      itemCount: 1)
+                ],
+              );
+            },
+            itemCount: data.length));
+  }
+}
